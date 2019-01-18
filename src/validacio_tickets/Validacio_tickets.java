b52b4155a3f9cf9086d2eb0d3fd572ced09d87e6
jfebrerc <jfebrerc@gmail.com>
2019-01-18 16:49:23 +0100

    inicial

---
+++ src/validacio_tickets/Validacio_tickets.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package validacio_tickets;
+
+/**
+ *
+ * @author Jose
+ */
+public class Validacio_tickets {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
