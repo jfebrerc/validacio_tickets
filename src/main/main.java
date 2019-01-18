07cf770546cce4bff0f1ed2adc77de6fdae07ed4
jfebrerc <jfebrerc@gmail.com>
2019-01-18 16:54:11 +0100

    .

--- src/main/main.java
+++ src/main/main.java
@@ -3,19 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package validacio_tickets;
+package main;
 
 /**
  *
  * @author Jose
  */
-public class Validacio_tickets {
+public class main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("patata");
     }
     
 }
